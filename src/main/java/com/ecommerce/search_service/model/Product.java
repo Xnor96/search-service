8a76a7f03cf06b372f7559a046a7b04fed47bb02
Xnor96 <mariopachecoherrada@gmail.com>
2025-03-03 17:37:09 -0700

    adding changes

--- src/main/java/com/ecommerce/search_service/model/Product.java
+++ src/main/java/com/ecommerce/search_service/model/Product.java
@@ -1,15 +1,14 @@
 package com.ecommerce.search_service.model;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.elasticsearch.annotations.Document;
-import org.springframework.data.elasticsearch.annotations.Field;
-import org.springframework.data.elasticsearch.annotations.FieldType;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Document(indexName = "products")
+@Entity
+@Table(name = "products")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -17,23 +16,11 @@ public class Product {
     @Id
     private String id;
     
-    @Field(type = FieldType.Text, name = "name")
     private String name;
-    
-    @Field(type = FieldType.Text, analyzer = "spanish", searchAnalyzer = "spanish")
     private String description;
-    
-    @Field(type = FieldType.Keyword)
     private String category;
-    
-    @Field(type = FieldType.Keyword)
     private String brand;
-    
-    @Field(type = FieldType.Double)
     private Double price;
-    
-    @Field(type = FieldType.Integer)
     private Integer stock;
-    
     private String imageUrl;
 }
--- src/main/java/com/ecommerce/search_service/repository/ProductRepository.java
+++ src/main/java/com/ecommerce/search_service/repository/ProductRepository.java
@@ -1,19 +1,21 @@
 package com.ecommerce.search_service.repository;
 
-import java.util.List;
-
-import org.springframework.data.elasticsearch.annotations.Query;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import com.ecommerce.search_service.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import com.ecommerce.search_service.model.Product;
+import java.util.List;
 
 @Repository
-public interface ProductRepository extends ElasticsearchRepository<Product, String> {
+public interface ProductRepository extends JpaRepository<Product, String> {
+    
     List<Product> findByNameContainingOrDescriptionContaining(String name, String description);
+    
     List<Product> findByCategory(String category);
+    
     List<Product> findByBrand(String brand);
     
-    @Query("{\"bool\": {\"must\": [{\"match\": {\"description\": \"?0\"}}]}}")
-    List<Product> findByDescriptionMatching(String description);
+    @Query("SELECT p FROM Product p WHERE p.price BETWEEN ?1 AND ?2")
+    List<Product> findByPriceRange(Double minPrice, Double maxPrice);
 }
